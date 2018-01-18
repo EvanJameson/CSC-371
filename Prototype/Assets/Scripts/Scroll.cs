a8889f3a4cff2f9e3b80688e7ccd04fca47c9efd
Evan Jameson <ecjameson@gmail.com>
2018-01-18 12:53:58 -0800

    Adding background
    Basically its a placeholder background that scrolls automatically forever, we can use it in our levels til we figure out what we want to use. Maybe we can add parallax scrolling later?
    
    I got the idea from this guy:
    https://twitter.com/shaunspalding/status/952203033152245766
    
    We could lay out our levels like this with super simple placeholder sprites then spruce things up later
    
    I can show you guys how to make it at the meeting tonight if you want

---
+++ Prototype/Assets/Scripts/Scroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+	public float speed = 0.05f;
+	private Renderer rd;
+
+	// Use this for initialization
+	void Start () {
+		rd = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (Time.time * speed, Time.time * speed);
+		rd.material.mainTextureOffset = offset;
+	}
+}
