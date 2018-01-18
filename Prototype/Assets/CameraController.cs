6b56a12dd91ffcbe1273d7ba0fa44db85fac7c51
Evan Jameson <ecjameson@gmail.com>
2018-01-18 15:23:01 -0800

    Big Update
    Added placeholder sprites for player and tiles
    
    player can walk, sprint, and jump
    jump height also varies based on whether or not youre sprinting
    
    also fixed an issue where you could stick to the sides of the tiles by adding the "slick" material
    
    also made the Tiles prefab
    
    also tracked the camera to the player but it could be better

---
+++ Prototype/Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		//https://answers.unity.com/questions/174002/what-is-the-relationship-between-camera-size-units.html
+		//believe this is so things will scale correctly if the resolution is changed, not sure
+		offset = transform.position - player.transform.position;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+}
