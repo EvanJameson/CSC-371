d2bc8eade8e4bc22103f0cf8bd69b3b7c3e612ac
vwbaker <toriiii37@gmail.com>
2018-02-06 10:01:12 -0800

    Added the ability to change scenes and complete level 1

---
+++ Prototype/Assets/Scripts/CompleteLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CompleteLevel : MonoBehaviour {
+
+	public string nextLevel;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			print ("in trigger");
+			SceneManager.LoadScene (nextLevel);
+		}
+	}
+}
