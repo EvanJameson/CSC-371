9cb9bbd5b434905eeb1ccebfb6bcc9328cad674d
Braden <bradensaito@gmail.com>
2018-02-12 17:48:06 -0800

    Started tutorial for level 1

---
+++ Prototype/Assets/Tutorial_1_Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_1_Manager : MonoBehaviour {
+
+    public static bool isStart = false;
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space"))
+        {
+            Time.timeScale = 1;
+        }
+	}
+}
