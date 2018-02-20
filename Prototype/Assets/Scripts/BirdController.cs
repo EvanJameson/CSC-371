b415212b6a7339a62e28fabde30766d4077bb03d
vwbaker <toriiii37@gmail.com>
2018-02-20 11:56:41 -0800

    Start to adding a bird to swoop and attack cat

---
+++ Prototype/Assets/Scripts/BirdController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	private bool attack = false;
+	private float bird_speed = 0.5f;
+
+	void Start() {
+		rb = GetComponentInChildren<Rigidbody2D> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("Player")) {
+			StartBirdAttack ();
+		}
+	}
+
+	void StartBirdAttack() {
+		attack = true;
+	}
+
+	void Update() {
+		if (attack) {
+			Vector2 v = CharacterControl.instance.player.transform.position - rb.transform.position;
+			v.Normalize ();
+			rb.velocity = v * bird_speed;
+			rb.transform.rotation = new Quaternion (0, 0, Mathf.PI/3f - Mathf.Atan(v.x / v.y), 1);
+		}
+	}
+}
