0e3eac7e2617b852727dcd71001d35fe5b60a283
EvanJameson <ecjameson@gmail.com>
2018-02-20 12:48:34 -0800

    Level switch
    Can actually advance between levels

--- Prototype/Assets/Scripts/CompleteLevel.cs
+++ Prototype/Assets/Scripts/CompleteLevel.cs
@@ -10,6 +10,8 @@ public class CompleteLevel : MonoBehaviour {
 	public void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.CompareTag ("Player")) {
 			SceneManager.LoadScene (nextLevel);
+			print ("what");
 		}
+		print ("its the tag");
 	}
 }
