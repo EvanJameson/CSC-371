5ab5c7f400490d9de27ac3dec87f4549f3efa370
nshockwa <nick.sciacqua@gmail.com>
2018-02-21 19:36:15 -0800

    modified audio manager and canvas. TODO: figure out a way to make UI consistent between level changes

--- Prototype/Assets/Scripts/ChewAbility.cs
+++ Prototype/Assets/Scripts/ChewAbility.cs
@@ -4,14 +4,12 @@ using System.Collections;
 [CreateAssetMenu (menuName = "Abilities/ChewAbility")]
 public class ChewAbility : Ability {
 	
-	private ChewTriggerable chewer;
+	//private ChewTriggerable chewer;
 	public override void Initialize(GameObject obj)
 	{
-		chewer = obj.GetComponent<ChewTriggerable> ();
 	}
 
 	public override void TriggerAbility()
 	{
-		chewer.Chew();
 	}
 }
--- Prototype/Assets/Scripts/ChewTriggerable.cs
+++ Prototype/Assets/Scripts/ChewTriggerable.cs
@@ -3,17 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChewTriggerable : MonoBehaviour {
-	private Collider2D collider;
 	public void Chew()
 	{
-		if(collider .gameObject.CompareTag("Rope"))
-		{
-			collider.gameObject.SetActive (false);
-		}
 	}
 
-	public void OnTriggerStay2D(Collider2D other)
-	{
-		collider = other;
-	}
 }
