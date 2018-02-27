98c3d25e52f308c3f7bd1d5b7dd13f0d9f266cfd
EvanJameson <ecjameson@gmail.com>
2018-02-26 20:11:07 -0800

    Updating menu
    sliding around

---
+++ Prototype/Assets/MoveLeft.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour {
+
+	RectTransform rt;
+	bool move = false;
+	Vector3 slide;
+
+
+	// Use this for initialization
+	void Start () {
+		rt = this.gameObject.GetComponent<RectTransform> ();
+		slide = new Vector3 (-1.1f,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(move)
+		{
+			rt.Translate (slide);
+			slide.x += slide.x;
+			if(slide.x < -600)
+			{
+				//need to figure out how to stop the slide of the level buttons
+				move = false;
+				//this.gameObject.SetActive (false);
+			}
+			if(slide.x < 0 && this.gameObject.transform.position.x == -29.70679)
+			{
+				//move = false;
+			}
+		}
+	}
+
+	public void Move()
+	{
+		move = true;
+	}
+}
--- Prototype/Assets/Scripts/MainMenu.cs
+++ Prototype/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,19 @@ public class MainMenu : MonoBehaviour {
 
 	public void gameStart()
     {
+		//Maybe change this to previously loaded level?
         SceneManager.LoadScene("Level1");
     }
+
+	public void levelSelect()
+	{
+		//figure out how to store these inbetween game launches yo
+
+	}
+
+	public void exit()
+	{
+		//quit game
+		Application.Quit();
+	}
 }
