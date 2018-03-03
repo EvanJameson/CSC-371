751f1886200a32108721f571a88f5e00fbae51fd
root <pradeep.sangli@gmail.com>
2018-03-03 14:54:23 -0800

    cat jumping shockwave

---
+++ Prototype/Assets/Cat.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour {
+
+	public float forceY = 150f;
+	public float forceX = 75f;
+	private Rigidbody2D myRigidbody;
+	bool isgrounded = false;
+	bool hasWaved= false;
+
+
+	public GameObject PlayerBulletGO;
+	public GameObject PlayerBulletGO2;
+	public GameObject shock1;
+	public GameObject shock2;
+
+	void Awake(){
+		myRigidbody = GetComponent<Rigidbody2D> ();
+
+	}
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Jump ());
+	}
+
+	IEnumerator Jump(){
+		yield return new WaitForSeconds (Random.Range (3, 10));
+		//forceY = Random.Range (250, 400);
+		myRigidbody.AddForce (new Vector2 (0, 500f));
+		hasWaved = false;
+		yield return new WaitForSeconds (2.0f);
+		GameObject bullet01 = (GameObject)Instantiate (PlayerBulletGO);
+		bullet01.transform.position = shock1.transform.position;
+
+		GameObject bullet02 = (GameObject)Instantiate (PlayerBulletGO2);
+		bullet02.transform.position = shock2.transform.position;
+
+		StartCoroutine (Jump ());
+	}
+
+
+	/*
+	IEnumerator Move(){
+		yield return StartCoroutine (Jump());
+		forceX = 75f;
+		myRigidbody.AddForce (new Vector2 (forceX, 0));
+		//yield return new WaitForSeconds (3.5f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(isgrounded == true){
+			if(hasWaved == false){
+				StartCoroutine (Move ());
+				hasWaved = true;
+			}
+				
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.CompareTag("Floor")){
+			isgrounded = true;
+		}
+	}
+
+*/
+}
---
+++ Prototype/Assets/Scripts/catShockwave.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catShockwave : MonoBehaviour {
+
+	float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 8f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 position = transform.position;
+
+		position = new Vector2 (position.x + speed * Time.deltaTime, position.y );
+
+		transform.position = position;
+
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+		Debug.Log ("Max x: " + max.x, gameObject);
+
+		if(transform.position.x > 18 ){
+			Destroy (gameObject);
+		}
+	}
+}
