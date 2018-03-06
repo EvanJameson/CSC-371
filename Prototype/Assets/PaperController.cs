027ec71a1ab68c5c928f76724f43a25afe7b5e18
vwbaker <toriiii37@gmail.com>
2018-03-05 16:37:30 -0800

    basic paper pick up

---
+++ Prototype/Assets/PaperController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperController : MonoBehaviour {
+
+	public GameObject xbutton;
+
+	private Vector3 offset = Vector3.zero;
+	private bool followCamera = false;
+	private bool animateIn = false;
+	private Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+		canvas = Object.FindObjectOfType<Canvas> ();
+	}
+	
+	void Update () {
+		if (followCamera) {
+			transform.position = CharacterControl.instance.player.transform.position + offset;
+		}
+		if (animateIn) {
+			if (transform.localScale.x < 0.7f) {
+				transform.RotateAround (transform.position, new Vector3 (0, 0, 1), -24.0f);
+				transform.localScale += new Vector3 (0.05f, 0.05f, 0.05f);
+			} else {
+				canvas.SendMessage ("RenderPaper");
+				animateIn = false;
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (!followCamera && other.CompareTag ("Player")) {
+			offset = transform.position - CharacterControl.instance.player.transform.position;
+			followCamera = true;
+			animateIn = true;
+			xbutton.SetActive (true);
+		}
+	}
+
+}
