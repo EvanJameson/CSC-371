fa34c8a60a446687b7ccc7b2869ff5aab4097d91
vwbaker <toriiii37@gmail.com>
2018-03-05 23:08:17 -0800

    Prevent buttons from being pushed by spacebar

--- Prototype/Assets/PaperCanvasController.cs
+++ Prototype/Assets/PaperCanvasController.cs
@@ -17,14 +17,16 @@ public class PaperCanvasController : MonoBehaviour {
 	private Vector2 startingSize;
 	private Vector3 deltaPosition;
 	private Vector2 deltaSize;
+
 	private float deltaTime = 2;
+	private float bias = 0.01f;
 
 	void Start () {
 		rt = GetComponent<RectTransform> ();
 	}
 	
 	void Update () {
-		if (deltaTime <= 1 + 0.01) {
+		if (deltaTime <= 1 + bias) {
 			rt.position = startingPoint + deltaPosition * deltaTime;
 			rt.sizeDelta = startingSize + deltaSize * deltaTime;
 			deltaTime += 0.1f;
@@ -41,25 +43,32 @@ public class PaperCanvasController : MonoBehaviour {
 		
 	void OnClick() {
 		if (active && expanded) {
+			/* Page is enlarged and expanded UI is in the background -
+			 * Move back to original place on expanded UI */
 			Animate (startingPoint, miniSize);
 			active = false;
 		} else if (active) {
+			/* Must have just been picked up - move to bottom corner */
 			Animate (bottomRight, miniSize);
 			active = false;
 		} else if (expanded) {
+			/* On the expanded UI, enlarge page for user to read */
 			Animate (centerScreen, expandedSize);
 			active = true;
 		} else {
+			/* Clicked on bottom right corner requesting expanded UI */
 			expanded = true;
 			SendMessageUpwards ("ExpandJournalUI");
 		}
 	}
 
+	/* Called from Storyline when all pieces should return to bottom right corner */
 	void ReturnInactive() {
 		Animate (bottomRight, miniSize);
 		expanded = false;
 	}
 
+	/* Called from Storyline when necessary to move into place of expanded UI */
 	void MoveTo(Vector3 destination) {
 		Animate (destination, miniSize);
 	}
