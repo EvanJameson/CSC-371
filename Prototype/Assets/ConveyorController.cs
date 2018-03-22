05969db360fc150b2a5b7c11875c185da5b1cbc9
Braden <bradensaito@gmail.com>
2018-03-21 17:42:43 -0700

    basic movement of first obstacle

---
+++ Prototype/Assets/ConveyorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorController : MonoBehaviour {
+
+    public float conveyorSpeed;
+    public bool rightFace;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("in");
+            GameObject player = collision.gameObject;
+            if(rightFace)
+            {
+                Vector2 moveDir = new Vector2(conveyorSpeed, 0);
+                collision.gameObject.GetComponent<Rigidbody2D>().velocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity + moveDir;
+            }
+            else
+            {
+                Vector2 moveDir = new Vector2(-conveyorSpeed, 0);
+                player.GetComponent<Rigidbody2D>().AddForce(moveDir);
+            }
+            
+        }
+    }
+}
---
+++ Prototype/Assets/LabBossController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabBossController : MonoBehaviour {
+
+    public bool isRight;
+    private int count;
+
+    public GameObject obj1;
+    public GameObject obj2;
+
+    public Transform ll1;
+    public Transform ll2;
+    public Transform ll3;
+
+    public Transform rl1;
+    public Transform rl2;
+    public Transform rl3;
+
+	// Use this for initialization
+	void Start () {
+        isRight = false;
+        count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void FixedUpdate()
+    {
+        if (count >= 180)
+        {
+            Instantiate(obj1, rl1);
+            count = 0;
+        }
+        count++;
+    }
+}
